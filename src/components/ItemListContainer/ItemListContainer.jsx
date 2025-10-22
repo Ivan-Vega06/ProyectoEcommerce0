ecc3c36de58afb90e3d6d9faaaba3b585e92db4e
Ivan-Vega06 <mauriivan324@gmail.com>
2025-10-22 19:36:25 -0300

    Error de import fix v2

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { ItemList } from "../ItemList/ItemList.jsx";
+import { ItemList } from "../ItemList/itemList.jsx";
 
 export const ItemListContainer = ({ titulo }) => {
   const [products, setProducts] = useState([]);
